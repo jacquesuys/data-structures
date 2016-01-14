f82955a5f0ccd5ac8007a24d6fa45584b3908f6a
jacquesuys <jaxuys@gmail.com>
2016-01-14 10:57:24 -0800

    Implements the prototypal instantiation style for a queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+
+  someInstance.counter = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.counter;
+  },
+  enqueue: function(value) {
+    this.counter++;
+    this[this.counter] = value;
+  },
+  dequeue: function() {
+    if (this.counter > 0) {
+      var dequeued = this[1];
+      delete this[1];
+      this.counter--;
+      for (var key in this) {
+        this[key-1] = this[key];
+      }
+      return dequeued;
+    }
+  }
+};
 
 
