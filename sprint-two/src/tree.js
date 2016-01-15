ffec879887ffc3e68df29b0d33ad949180621a75
jacquesuys <jaxuys@gmail.com>
2016-01-15 11:17:45 -0800

    Adds children to the tree, and detects existence

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,11 +12,21 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
-};
+treeMethods.contains = function(target, currentTree) {
+  currentTree = currentTree || this;
 
+  if (currentTree.value === target) {
+    return true;
+  } else if(this.children) {
+    for (var i = 0; i < this.children.length; i++) {
+      return treeMethods.contains(target, this.children[i]);
+    }
+  }
+  return false;
+};
 
 
 /*
