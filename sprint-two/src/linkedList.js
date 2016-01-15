7e83507f7b1ccaf08569186c476646c8cac1219c
jacquesuys <jaxuys@gmail.com>
2016-01-14 19:14:59 -0800

    Adds to the tail of list when addToTail called and removes head from list when removeHead is called

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,48 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list._length = 0;
 
   list.addToTail = function(value) {
+    var linkedListNode = new Node(value);
+    var currentNode = list.head;
+
+    list.tail = linkedListNode;
+    
+    if (!currentNode) {
+      list.head = linkedListNode;
+      list._length++;
+      return linkedListNode;
+    }
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = linkedListNode;
+
+    list._length++;
+
+    return linkedListNode;
   };
 
   list.removeHead = function() {
+    var currentNode = this.head;
+    var deletedNode = null;
+    if (currentNode) {
+      list.head =  currentNode.next;
+      deletedNode = currentNode;
+      currentNode = null;
+      list._length--;
+
+      return deletedNode;
+    }
   };
 
   list.contains = function(target) {
   };
 
+  console.log(list);
   return list;
 };
 
