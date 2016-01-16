07ed0fad223b278aafd0637081c32e0cbce4ca04
jacquesuys <jaxuys@gmail.com>
2016-01-16 11:22:28 -0800

    Creates a set data structure and operates accordingly. Time complexity notes included.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item]) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item]) {
+    delete this._storage[item];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(1) / Constant time
+ * contains: O(1) / Constant time
+ * remove: O(1) / Constant time
  */
