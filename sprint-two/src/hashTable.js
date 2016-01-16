35c4574d88ec6572590eb097d5fb83111acc6244
jacquesuys <jaxuys@gmail.com>
2016-01-16 15:32:31 -0800

    Implements a working hash table with insert, retreive, and remove methods. Includes notes about time complexity

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,21 +7,50 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  var bucket = this._storage.get(index) || [];
+  var tuple = [k, v];
+  var overRidden = false;
+
+  for (var i = 0; i < bucket.length; i++) {
+      var currentTuple = bucket[i];
+      if (currentTuple[0] === k) {
+        currentTuple[1] = v;
+        overRidden = true;
+      }
+  }
+  if (!overRidden) {
+    bucket.push(tuple);
+  }
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = undefined;
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: O(n) / linear time
+ * retrieve: O(n) / linear time
+ * remove: O(n) / linear time
  */
